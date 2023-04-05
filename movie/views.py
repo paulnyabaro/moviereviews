06b77e34003f68823e52ae7a52537f29fb633aa9
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-05 07:09:21 +0300

    Added about view

--- movie/views.py
+++ movie/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Welcome to the homepage</h1>')
+
+def about(request):
+    return HttpResponse('<h1>This is the about page</h1>')
