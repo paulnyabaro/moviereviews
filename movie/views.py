d33a9d82051542d900eb297630e5836145e81dae
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-05 07:18:15 +0300

    Added home template

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Welcome to the homepage</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return HttpResponse('<h1>This is the about page</h1>')
