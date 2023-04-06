557c6b89731b59f0e34a3196736487d59bc36308
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-06 06:48:15 +0300

    Ordering news by date

--- news/views.py
+++ news/views.py
@@ -3,5 +3,5 @@ from .models import News
 
 
 def news(request):
-    newss = News.objects.all()
+    newss = News.objects.all().order_by('-date')
     return render(request, 'news.html', {'newss':newss})
