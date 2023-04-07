e9286ced0a91600c7bb267a4cd396fe1d8bc5c1a
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-07 07:24:02 +0300

    Added user creation

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import redirect
 def signup(request):
     if request.method == 'GET':
         return render(request, 'signup-account.html', {'form': UserCreationForm})
+    
     else:
         if request.POST['password1'] == request.POST['password2']:
             user = User.objects.create_user(request.POST['username'],
@@ -15,6 +16,9 @@ def signup(request):
             user.save()
             login(request, user)
             return redirect('home')
+        
+        else:
+            return render(request, 'signup-account.html', {'form': UserCreationForm, 'error': 'Password do not match'})
 
 
 
