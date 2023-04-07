811538f268e7ba050150603fcb9699d5b8b9ec8b
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-07 07:17:37 +0300

    Added user creation

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.shortcuts import redirect
+
 
 def signup(request):
-    return render(request, 'signup-account.html', {'form': UserCreationForm})
+    if request.method == 'GET':
+        return render(request, 'signup-account.html', {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'],
+                                            password=request.POST['password1'])
+            user.save()
+            login(request, user)
+            return redirect('home')
 
 
 
