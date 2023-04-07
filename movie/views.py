bdc20ff81f6a0f9379b9559ff765a08670f7252f
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-07 06:16:29 +0300

    Added movie detail view

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from .models import Movie
 
 
@@ -17,3 +18,7 @@ def signup(request):
 
 def about(request):
     return HttpResponse('<h1>This is the about page</h1>')
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'detail.html', {'movie': movie})
