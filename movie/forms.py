ab6d84b926f4252192a0c088a19b4c3f4ffefa38
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-08 09:38:14 +0300

    Updated review form

--- movie/forms.py
+++ movie/forms.py
@@ -8,12 +8,12 @@ class ReviewForm(ModelForm):
         self.fields['text'].widget.attrs.update({'class': 'form-control'})
         self.fields['watchAgain'].widget.attrs.update({'class': 'form-check-input'})
 
-        class Meta:
-            model = Review
-            fields = ['text', 'watchAgain']
-            labels = {
-                'watchAgain': ('Watch Again')
-            }   
-            widgets = {
-                'text': Textarea(attrs={'rows':4}),
-            }
+    class Meta:
+        model = Review
+        fields = ['text', 'watchAgain']
+        labels = {
+            'watchAgain': ('Watch Again')
+        }   
+        widgets = {
+            'text': Textarea(attrs={'rows':4}),
+        }
