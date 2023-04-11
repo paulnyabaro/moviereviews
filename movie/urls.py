219d084b3009e654fc31b5fff89c21d1fd73e110
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-11 21:42:52 +0300

    Added update revew url

--- movie/urls.py
+++ movie/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('<int:movie_id>/', views.detail, name='detail'),
     path('<int:movie_id>/create/', views.create_review, name='create_review'),
+    path('review/<int:review_id>/', views.update_review, name='update_review'),
 ]
