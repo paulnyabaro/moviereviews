65fd264eaa9a4fab77fc2b02a01c6b94c46fb734
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-12 06:14:21 +0300

    Working on delete review

--- movie/urls.py
+++ movie/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:movie_id>/', views.detail, name='detail'),
     path('<int:movie_id>/create/', views.create_review, name='create_review'),
     path('review/<int:review_id>/', views.update_review, name='update_review'),
+    path('review/<int:review_id>/delete/', views.delete_review, name='delete_review'),
 ]
